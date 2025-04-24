4c284eefac4515dee563c55d5056c52453fb7a68
Federico Spadaro <federicospadaro2@gmail.com>
2025-04-24 18:14:57 +0200

    [ADD] create snack2 HTML and JavaScript files

---
+++ snack2/snack2.js
@@ -0,0 +1,14 @@
+// 🏆 Snack 2
+// Crea una arrow function che calcola il quadrato di un numero.
+
+const quadrato = (num) => num * num;
+console.log(quadrato(10));
+
+
+// Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+
+
+// Inizialmente ho pensato subito all'arrow funtion per abbreviare la scrittura, ma ho deciso di scrivere la funzione in modo tradizionale per completezza, rispettando comunque l'esercizio
+
+function quadrato(num) { return num * num; }  
+console.log(quadrato(10));
