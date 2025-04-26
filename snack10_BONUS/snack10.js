a227ae762567a2bf0b69e65964418b457ac7f40f
Federico Spadaro <federicospadaro2@gmail.com>
2025-04-26 15:54:02 +0200

    [UPDATE] remove example usage comments from snack10.js

--- snack10_BONUS/snack10.js
+++ snack10_BONUS/snack10.js
@@ -1,12 +1,6 @@
 // Scrivi una funzione creaThrottler che accetta una funzione e un tempo `limite`.
 
 // Restituisce una nuova funzione che, quando chiamata ripetutamente, esegue l'operazione originale al massimo una volta ogni n millisecondi.
-// Esempio di utilizzo:
-// const throttledLog = creaThrottler(() => console.log("Eseguito!"), 2000);
-// ​
-// throttledLog(); // ✅ "Eseguito!"
-// throttledLog(); // ❌ Ignorato (chiamato troppo presto)
-// setTimeout(throttledLog, 2500); // ✅ "Eseguito!" (dopo 2.5 secondi)
 
 function creaThrottler(mainfunction, delay) {
     let lastTimeExecuted = 0;
